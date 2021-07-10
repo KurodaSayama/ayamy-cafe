fbba658848145cc928e6070ca76c8474e461fbbb
Fancy Fish <fancyfish.steam@gmail.com>
2021-07-10 16:08:11 +0100

    #1 Recipe Cards initial + data for Oyakodon

--- src/ImageList.tsx
+++ src/ImageList.tsx
@@ -13,8 +13,17 @@ import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import SwipeableViews from 'react-swipeable-views';
 import YouTube from 'react-youtube';
 import itemData from './itemData';
+import QRCode from  'qrcode.react';
+import ReactToPrint from "react-to-print";
+import IconButton from '@material-ui/core/IconButton';
+import PrintIcon from '@material-ui/icons/Print';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,6 +47,49 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  recipeImg: {
+    width: '100%',
+    marginTop: '64px',
+  },
+  recipeCardHeader: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexFlow: 'row',
+    justifyContent: 'space-between',
+  },
+  recipeCardLogo: {
+    paddingBottom: '20px',
+    display: 'flex',
+    flexFlow: 'row',
+    color: '#cfb1b0',
+  },
+  modalRightContent: {
+    width: '100%',
+    overflow: 'auto'
+  },
+  tabs: {
+    justifyItems: 'flex-end',
+  },
+  ingredients: {
+    margin: '10px',
+    padding: '10px',
+    backgroundColor: '#FFE1E3',
+    borderRadius: '6px',
+  },
+  steps: {
+    margin: '10px',
+    padding: '10px',
+  },
+  step: {
+    marginBottom:'10px',
+    fontSize: '12px',
+  },
+  icon: {
+    margin: 'auto',
+  },
+  firstTab: {
+    marginLeft: 'auto',
+  },
   paper: {
     backgroundColor: theme.palette.background.paper,
     border: '20px solid #ffe1e0',
@@ -45,8 +97,149 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
     width: '100%',
     outline: 'none',
+    maxHeight: '90vh',
+    overflow: 'auto',
   },
 }));
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`recipe-tabpanel-${index}`}
+      aria-labelledby={`recipe-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={2}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `recipe-tab-${index}`,
+    'aria-controls': `recipe-tabpanel-${index}`,
+  };
+}
+
+function RecipeTabs (props: any) {
+  const item = props.item
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  let printRef: any;
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.modalRightContent}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="standard"
+          aria-label="Recipe Tabs"
+          className={classes.tabs}
+        >
+          <Tab className={classes.firstTab} label="Video" {...a11yProps(0)} />
+          {item.recipeCard &&
+          <Tab label="Recipe" {...a11yProps(1)} />
+          }
+        </Tabs>      
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          disableLazyLoading
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <YouTube videoId={item.videoId} containerClassName={'youtubeContainer'} />
+          </TabPanel>
+          <TabPanel  value={value} index={1} dir={theme.direction}>
+            <div ref={(el) => (printRef = el)}>
+              <div className={classes.recipeCardHeader}>
+                <div>
+                  <div className={classes.recipeCardLogo}>
+                    <FontAwesomeIcon icon={faCoffee} size="4x" className={classes.icon} />
+                    <Box m={1} />
+                    <Grid container>
+                      <Grid item xs={12}>
+                        <Typography variant="h4" color="inherit" noWrap>
+                          あやみカフェ
+                      </Typography>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography variant="h5" color="inherit" noWrap>
+                          Ayamy Cafe
+                      </Typography>
+                      </Grid>
+                    </Grid>
+                  </div>
+
+                  <Typography variant="h6" align="left">
+                  <Box display="none" displayPrint="box">
+                    {item.title}
+                  </Box>
+                  <Box displayPrint="none">
+                    {item.title}
+                    <ReactToPrint
+                      trigger={() => <IconButton aria-label="print">
+                        <PrintIcon/>
+                      </IconButton>}
+                      content={() => printRef}
+                    />
+                  </Box>
+                   
+                  </Typography>
+                </div>
+                
+                <QRCode size={128} includeMargin fgColor="#cfb1b0" value={"http://www.youtube.com/watch?v=" + item.videoId} />
+              </div>
+              <div className={classes.ingredients}>
+                <h4>Ingredients</h4>
+                <div>
+                {item.ingredientsEN.map((ingredient: any, index: number) => (
+                  <span key={"ingredient-"+index}>{ingredient.name}, </span>
+                ))}
+                </div>
+              </div>
+              <div className={classes.steps}>
+                <h4>Steps</h4>
+                <div>
+                {item.stepsEN.map((step: any, index: number) => (
+                  <div key={"step-"+index} className={classes.step}>{index + 1}. {step.step}</div>
+                ))}
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+        </SwipeableViews>
+      </div>
+    </div>
+  );
+}
 
 export default function SingleLineImageList() {
   const classes = useStyles();
@@ -56,6 +249,8 @@ export default function SingleLineImageList() {
   const lg = useMediaQuery(theme.breakpoints.up('lg'));
   const md = useMediaQuery(theme.breakpoints.up('md'));
   const sm = useMediaQuery(theme.breakpoints.up('sm'));
+
+  
 
   var cols;
   if (xl) {
@@ -125,17 +320,19 @@ export default function SingleLineImageList() {
               <Typography id={`transition-modal-title-${index}`} variant="h5" align="center">
                 {item.title}
               </Typography>
+              {item.price !== '????' &&
               <Typography id={`transition-modal-description-${index}`} variant="body2" align="center" gutterBottom>
                 価格：{item.price}
               </Typography>
+              }
               <Grid container spacing={4}>
-                <Grid item xs={12} sm={item.modalCols as GridSize}>
-                  <Box display={{ xs: 'none', md: 'block' }}>
-                    <img src={item.img} alt={item.title} width='100%' />
+                <Grid item xs={12} sm={12} md={item.modalCols as GridSize}>
+                  <Box display={{ xs: 'none', sm: 'none', md: 'block' }}>
+                    <img src={item.img} alt={item.title} className={classes.recipeImg} />
                   </Box>
                 </Grid>
-                <Grid item xs={12} sm={(12 - item.modalCols) as GridSize}>
-                  <YouTube videoId={item.videoId} containerClassName={'youtubeContainer'} />
+                <Grid item xs={12} sm={12} md={(12 - item.modalCols) as GridSize}>
+                  <RecipeTabs item={item}></RecipeTabs>
                 </Grid>
               </Grid>
             </Container>
